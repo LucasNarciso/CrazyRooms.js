0d30ec4d2798e2bf6d9afa95091045928522423c
Lucas Narciso <lucasnarcisoo@gmail.com>
2024-06-02 21:16:45 -0300

    Alterando funções de paginação para se adequar ao Localhost e GHPages

--- GlobalScript.js
+++ GlobalScript.js
@@ -1,36 +1,47 @@
 //Leva o usuário para uma página específica
 function goOn(voltar, page){
-    console.log(page)
-    console.log(voltar)
+
+    let urlsplit = window.location.href.split('/')
+
     switch (page) {
         case "menu":
             document.querySelector('.conteiner').classList.add('slideOut')
             setTimeout(() => {
-                window.location.href = "/menu"
+                // window.location.href = "Menu"
+                urlsplit[urlsplit.indexOf(urlsplit.at(-2))] = "Menu"
+                window.location.href = urlsplit.join('/')
             }, 200);
             break;
         case "personagens":
             document.querySelector('.conteiner').classList.add('slideOut')
             setTimeout(() => {
-                window.location.href = "/personagens"
+                // window.location.href = "Personagens"
+                urlsplit[urlsplit.indexOf(urlsplit.at(-2))] = "Personagens"
+                window.location.href = urlsplit.join('/')
             }, 200);
             break;
         case "jogo":
             document.querySelector('.conteiner').classList.add('slideOut')
             setTimeout(() => {
-                window.location.href = "/jogo"
+                // window.location.href = "Jogo"
+                urlsplit[urlsplit.indexOf(urlsplit.at(-2))] = "Jogo"
+                window.location.href = urlsplit.join('/')
             }, 200);
             break;
         case "opcoes":
             document.querySelector('.conteiner').classList.add('slideOut')
             setTimeout(() => {
-                window.location.href = "/opcoes"
+                // window.location.href = "Opcoes"
+                urlsplit[urlsplit.indexOf(urlsplit.at(-2))] = "Opcoes"
+                window.location.href = urlsplit.join('/')
             }, 200);
             break;
         case "sobre":
             document.querySelector('.conteiner').classList.add('slideOut')
             setTimeout(() => {
-                window.location.href = "/opcoes"
+                // window.location.href = "Sobre"
+                urlsplit[urlsplit.indexOf(urlsplit.at(-2))] = "Sobre"
+                window.location.href = urlsplit.join('/')
             }, 200);
             break;
         default:
