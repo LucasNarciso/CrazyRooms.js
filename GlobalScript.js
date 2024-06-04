9230ca87622526065a5a21da20a126a018d37cd0
Lucas Narciso <lucasnarcisoo@gmail.com>
2024-06-03 23:00:38 -0300

    Melhorando paginação e adicionando animação para digitar textos do jogo no terminal

--- GlobalScript.js
+++ GlobalScript.js
@@ -1,47 +1,35 @@
 //Leva o usuário para uma página específica
 function goOn(voltar, page){
 
-    let urlsplit = window.location.href.split('/')
-
     switch (page) {
         case "menu":
             document.querySelector('.conteiner').classList.add('slideOut')
             setTimeout(() => {
-                // window.location.href = "Menu"
-                urlsplit[urlsplit.indexOf(urlsplit.at(-2))] = "Menu"
-                window.location.href = urlsplit.join('/')
+                window.location.href = "/CrazyRooms.js/Menu"
             }, 200);
             break;
         case "personagens":
             document.querySelector('.conteiner').classList.add('slideOut')
             setTimeout(() => {
-                // window.location.href = "Personagens"
-                urlsplit[urlsplit.indexOf(urlsplit.at(-2))] = "Personagens"
-                window.location.href = urlsplit.join('/')
+                window.location.href = "/CrazyRooms.js/Personagens"
             }, 200);
             break;
         case "jogo":
             document.querySelector('.conteiner').classList.add('slideOut')
             setTimeout(() => {
-                // window.location.href = "Jogo"
-                urlsplit[urlsplit.indexOf(urlsplit.at(-2))] = "Jogo"
-                window.location.href = urlsplit.join('/')
+                window.location.href = "/CrazyRooms.js/Jogo"
             }, 200);
             break;
         case "opcoes":
             document.querySelector('.conteiner').classList.add('slideOut')
             setTimeout(() => {
-                // window.location.href = "Opcoes"
-                urlsplit[urlsplit.indexOf(urlsplit.at(-2))] = "Opcoes"
-                window.location.href = urlsplit.join('/')
+                window.location.href = "/CrazyRooms.js/Opcoes"
             }, 200);
             break;
         case "sobre":
             document.querySelector('.conteiner').classList.add('slideOut')
             setTimeout(() => {
-                // window.location.href = "Sobre"
-                urlsplit[urlsplit.indexOf(urlsplit.at(-2))] = "Sobre"
-                window.location.href = urlsplit.join('/')
+                window.location.href =  "/CrazyRooms.js/Sobre"
             }, 200);
             break;
         default:
